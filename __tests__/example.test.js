f0d0f423714ccbd4893308ee38137fce5b4d7736
valtteriSut <119314232+valtteriSut@users.noreply.github.com>
2025-11-20 17:16:09 +0200

    Coveralls (#1)
    * add coveralls
    
    * add coveralls to yml
    
    * coveralls config
    
    * packages
    
    * coveralls config
    
    * node versio yml
    
    * coveralls config
    
    * add coveralls branch
    
    * add scripts package.json
    
    * coveralls config
    
    * coveralls config
    
    * check lcov.info
    
    * coveralls testing
    
    * coveralls secret test
    
    * coveralls secret update

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,3 +1,6 @@
-test("works", () => {
-  expect(2 + 2).toBe(4);
+// __tests__/example.test.js
+const { add } = require('../src/math');
+
+test("adds numbers", () => {
+  expect(add(2, 2)).toBe(4);
 });
---
+++ src/math.js
@@ -0,0 +1,6 @@
+// src/math.js
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { add };
